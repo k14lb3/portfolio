bb68df8b68202a91edb7f02878af215675bf0bf1
k14lb3 <k14lb3@gmail.com>
2022-04-01 23:49:05 +0800

    Add padding

--- src/components/background/index.tsx
+++ src/components/background/index.tsx
@@ -1,7 +1,11 @@
 import { FC } from "react";
 
 const Background: FC = ({ children }) => {
-  return <div className="relative h-full w-full bg-[#008081]">{children}</div>;
+  return (
+    <div className="relative h-full w-full bg-[#008081] pt-4 pb-12">
+      {children}
+    </div>
+  );
 };
 
 export default Background;
