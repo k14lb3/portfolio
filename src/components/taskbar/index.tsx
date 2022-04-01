c2519701c7aab9beae326dacf4f728ac1918aeaa
k14lb3 <k14lb3@gmail.com>
2022-04-02 00:03:19 +0800

    Make a separate component for the taskbar clock

--- src/components/taskbar/index.tsx
+++ src/components/taskbar/index.tsx
@@ -1,25 +1,11 @@
-import { FC, useState, useEffect } from "react";
+import { FC } from "react";
 import { useSetRecoilState } from "recoil";
-import { highlightState, startState } from "@/recoil/atoms";
+import { highlightState } from "@/recoil/atoms";
 import Start from "./start";
+import Clock from "./clock";
 
 const Taskbar: FC = () => {
   const setHighlightAtom = useSetRecoilState(highlightState);
-  const [time, setTime] = useState<string>("");
-
-  const setClock = () => {
-    const date = new Date();
-
-    setTime(
-      date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })
-    );
-  };
-
-  useEffect(() => {
-    setClock();
-
-    setInterval(() => setClock(), 60000);
-  }, []);
 
   return (
     <div
@@ -28,9 +14,7 @@ const Taskbar: FC = () => {
     >
       <div className="flex items-center justify-between relative h-full w-full pt-[2px] px-0.5">
         <Start />
-        <div className="h-6 w-[4.25rem] bg-taskbar-clock bg-no-repeat bg-cover text-center">
-          <time className="text-xs">{time}</time>
-        </div>
+        <Clock />
       </div>
     </div>
   );
