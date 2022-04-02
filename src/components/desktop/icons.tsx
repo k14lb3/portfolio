40824ecb5be60c952807146d9b81f5c2f26cd3be
k14lb3 <k14lb3@gmail.com>
2022-04-02 20:09:00 +0800

    Download resume on double click of Resume icon

--- src/components/desktop/icons.tsx
+++ src/components/desktop/icons.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useRef } from "react";
 import { useRecoilState, useSetRecoilState } from "recoil";
 import { highlightState, startState } from "@/recoil/atoms";
 
@@ -8,55 +8,59 @@ interface Icon {
   event: () => void;
 }
 
-const icons: Icon[] = [
-  {
-    index: 1,
-    filename: "recycle-bin",
-    event: () => {},
-  },
-  {
-    index: 2,
-    filename: "projects",
-    event: () => {},
-  },
-  {
-    index: 3,
-    filename: "resume",
-    event: () => {},
-  },
-  {
-    index: 4,
-    filename: "minecraft",
-    event: () => {},
-  },
-];
-
 export const Icons: FC = () => {
   const [highlightAtom, setHighlightAtom] = useRecoilState(highlightState);
   const setStartAtom = useSetRecoilState(startState);
+  const resumeRef = useRef<HTMLAnchorElement | null>(null);
+
+  const icons: Icon[] = [
+    {
+      index: 1,
+      filename: "recycle-bin",
+      event: () => {},
+    },
+    {
+      index: 2,
+      filename: "projects",
+      event: () => {},
+    },
+    {
+      index: 3,
+      filename: "resume",
+      event: () => resumeRef.current!.click(),
+    },
+    {
+      index: 4,
+      filename: "minecraft",
+      event: () => {},
+    },
+  ];
 
   return (
-    <div className="relative w-fit" onClick={() => setStartAtom(false)}>
-      {icons.map(({ index, filename, event }) => (
-        <div
-          key={filename}
-          className="h-16 w-[6.375rem] mb-4"
-          onClick={() => setHighlightAtom(index)}
-          onDoubleClick={() => {
-            event();
-            setHighlightAtom(0);
-          }}
-        >
-          <img
-            src={
-              highlightAtom === index
-                ? `/static/images/icons/highlighted/${filename}.png`
-                : `/static/images/icons/${filename}.png`
-            }
-            alt={filename}
-          />
-        </div>
-      ))}
-    </div>
+    <>
+      <a ref={resumeRef} href="/static/karlivan-alberto_resume.pdf" download />
+      <div className="relative w-fit" onClick={() => setStartAtom(false)}>
+        {icons.map(({ index, filename, event }) => (
+          <div
+            key={filename}
+            className="h-16 w-[6.375rem] mb-4"
+            onClick={() => setHighlightAtom(index)}
+            onDoubleClick={() => {
+              event();
+              setHighlightAtom(0);
+            }}
+          >
+            <img
+              src={
+                highlightAtom === index
+                  ? `/static/images/icons/highlighted/${filename}.png`
+                  : `/static/images/icons/${filename}.png`
+              }
+              alt={filename}
+            />
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
