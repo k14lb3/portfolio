842fe4a4b56ca9af3eec179426a6a2a226949f49
k14lb3 <k14lb3@gmail.com>
2022-04-05 22:23:17 +0800

    Change default cursor images

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    cursor: {
+      default: "url(/static/images/cursor/default.png), default",
+      pointer: "url(/static/images/cursor/pointer.png), pointer",
+      wait: "url(/static/images/cursor/wait.png), wait",
+      progress: "url(/static/images/cursor/progress.png), progress",
+      "ew-resize": "url(/static/images/cursor/ew-resize.png), ew-resize",
+      "ns-resize": "url(/static/images/cursor/ns-resize.png), ns-resize",
+      "nesw-resize": "url(/static/images/cursor/nesw-resize.png), nesw-resize",
+      "nwse-resize": "url(/static/images/cursor/nwse-resize.png), nwse-resize",
+    },
     extend: {
       fontFamily: {
         mss: ["Microsoft Sans Serif", ...defaultTheme.fontFamily.sans],
@@ -13,6 +23,14 @@ module.exports = {
         "start-default": "url(/static/images/taskbar/start/default.png)",
         "start-clicked": "url(/static/images/taskbar/start/clicked.png)",
         "start-menu": "url(/static/images/taskbar/start/menu/base.png)",
+      },
+      keyframes: {
+        blink: {
+          "0%": { opacity: 0 },
+        },
+      },
+      animation: {
+        blink: "blink 250ms linear infinite",
       },
     },
   },
