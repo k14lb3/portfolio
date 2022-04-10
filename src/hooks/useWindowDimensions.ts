44264e79ba50685cc348a015492b709d5277bc8c
k14lb3 <k14lb3@gmail.com>
2022-04-10 11:43:13 +0800

    Add useWindowDimensions hook

---
+++ src/hooks/useWindowDimensions.ts
@@ -0,0 +1,31 @@
+import { useLayoutEffect, useState } from "react";
+
+interface WindowDimensions {
+  width: number | undefined;
+  height: number | undefined;
+}
+
+const useWindowDimensions = (): WindowDimensions => {
+  const [windowDimensions, setWindowDimensions] = useState<WindowDimensions>({
+    width: undefined,
+    height: undefined,
+  });
+
+  useLayoutEffect(() => {
+    const resize = (): void => {
+      setWindowDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    resize();
+
+    window.addEventListener("resize", resize);
+    return (): void => window.removeEventListener("resize", resize);
+  }, []);
+
+  return windowDimensions;
+};
+
+export default useWindowDimensions;
