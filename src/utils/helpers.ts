374341921d5ed71387ef523583b2d086b1b64272
k14lb3 <k14lb3@gmail.com>
2022-04-15 19:07:38 +0800

    Add function that converts vh to px

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -10,6 +10,9 @@ export const generateRandomNumber = (
 export const convertPxToVh = (px: number, screenHeight: number) =>
   px * (100 / screenHeight);
 
+export const convertVhtoPx = (vh: number, screenHeight: number) =>
+  vh * (screenHeight / 100);
+
 export const arrowUpHandler = (
   setDesktopIconHighlightAtom: SetterOrUpdater<number>,
   startAtom: boolean,
