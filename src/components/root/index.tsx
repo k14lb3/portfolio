6dcc37645c7aa4638f9867f093a83639b7dd7ded
k14lb3 <k14lb3@gmail.com>
2022-04-18 12:26:32 +0800

    Move handle functions in the Root component instead of being in helpers file

--- src/components/root/index.tsx
+++ src/components/root/index.tsx
@@ -1,13 +1,13 @@
 import { FC, useEffect, useState } from "react";
-import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
-import _ from "lodash";
 import {
-  arrowUpHandler,
-  arrowDownHandler,
-  keydownEnterHandler,
-  keydownDefaultHandler,
-  generateRandomNumber,
-} from "@/utils/helpers";
+  SetterOrUpdater,
+  useRecoilState,
+  useRecoilValue,
+  useSetRecoilState,
+} from "recoil";
+import _ from "lodash";
+import { desktopIcons } from "@/utils/constants";
+import { generateRandomNumber } from "@/utils/helpers";
 import {
   bootState,
   windowsState,
@@ -19,6 +19,126 @@ import {
 } from "@/recoil/atoms";
 import Boot from "@/components/boot";
 import About from "@/components/about";
+import { options } from "@/components/taskbar/start-menu";
+
+export const handleArrowUp = (
+  setDesktopIconHighlightAtom: SetterOrUpdater<number>,
+  startAtom: boolean,
+  startMenuOptionHighlightAtom: SetterOrUpdater<number>
+): void => {
+  if (startAtom) {
+    startMenuOptionHighlightAtom((prevHighlight) => {
+      if (prevHighlight === 0 || prevHighlight === 1) return 4;
+
+      return --prevHighlight;
+    });
+  } else {
+    setDesktopIconHighlightAtom((prevHighlight) => {
+      if (prevHighlight === 0 || prevHighlight === 1) return 4;
+
+      return --prevHighlight;
+    });
+  }
+};
+
+export const handleArrowDown = (
+  setDesktopIconHighlightAtom: SetterOrUpdater<number>,
+  startAtom: boolean,
+  startMenuOptionHighlightAtom: SetterOrUpdater<number>
+): void => {
+  if (startAtom) {
+    startMenuOptionHighlightAtom((prevHighlight) => {
+      if (prevHighlight === 4) return 1;
+
+      return ++prevHighlight;
+    });
+  } else {
+    setDesktopIconHighlightAtom((prevHighlight) => {
+      if (prevHighlight === 4) return 1;
+
+      return ++prevHighlight;
+    });
+  }
+};
+
+export const handleKeydown = (
+  desktopIconsRefAtom: HTMLDivElement[],
+  desktopIconHighlightAtom: number,
+  startAtom: boolean,
+  startMenuOptionsRefAtom: HTMLDivElement[],
+  startMenuOptionHighlightAtom: number
+): void => {
+  if (startAtom) {
+    if (startMenuOptionHighlightAtom === 0) return;
+
+    startMenuOptionsRefAtom[startMenuOptionHighlightAtom - 1].click();
+  } else {
+    if (desktopIconHighlightAtom === 0) return;
+
+    const dblclick = new MouseEvent("dblclick", {
+      view: window,
+      bubbles: true,
+    });
+
+    desktopIconsRefAtom[desktopIconHighlightAtom - 1].dispatchEvent(dblclick);
+  }
+};
+
+export const handleDefaultKeydown = (
+  key: string,
+  desktopIconHighlightAtom: number,
+  setDesktopIconHighlightAtom: SetterOrUpdater<number>,
+  startAtom: boolean,
+  setStartMenuOptionHighlightAtom: SetterOrUpdater<number>
+): void => {
+  key = key.toLowerCase();
+
+  if (startAtom) {
+    let keyIndex: number = 0;
+
+    const optionKeys = options.map(({ index, label }) => ({
+      index: index,
+      key: label[0].toLowerCase(),
+    }));
+
+    const optionKey = optionKeys.filter((optionKey) => optionKey.key === key);
+
+    if (optionKey.length === 0) return;
+
+    keyIndex = optionKey[0].index;
+
+    setStartMenuOptionHighlightAtom(keyIndex);
+  } else {
+    let keyIndex: number = 0;
+
+    const shortcutKeys = desktopIcons.map(({ index, label }) => ({
+      index: index,
+      key: label[0].toLowerCase(),
+    }));
+
+    const shortcutKey = shortcutKeys.filter(
+      (shortcutKey) => shortcutKey.key === key
+    );
+
+    if (shortcutKey.length === 0) return;
+
+    if (shortcutKey.length > 1) {
+      if (desktopIconHighlightAtom !== shortcutKey[0].index) {
+        keyIndex = shortcutKey[0].index;
+      } else {
+        shortcutKey.forEach((key, i) => {
+          if (desktopIconHighlightAtom === key.index) {
+            keyIndex = shortcutKey[(i + 1) % shortcutKey.length].index;
+          }
+        });
+      }
+    } else {
+      keyIndex = shortcutKey[0].index;
+    }
+
+    setDesktopIconHighlightAtom(keyIndex);
+  }
+};
 
 const Root: FC = ({ children }) => {
   const bootAtom = useRecoilValue(bootState);
@@ -52,21 +172,21 @@ const Root: FC = ({ children }) => {
     const keydownEvents = (e: KeyboardEvent) => {
       switch (e.key) {
         case "ArrowUp":
-          arrowUpHandler(
+          handleArrowUp(
             setDesktopIconHighlightAtom,
             startAtom,
             setStartMenuOptionHighlightAtom
           );
           break;
         case "ArrowDown":
-          arrowDownHandler(
+          handleArrowDown(
             setDesktopIconHighlightAtom,
             startAtom,
             setStartMenuOptionHighlightAtom
           );
           break;
         case "Enter":
-          keydownEnterHandler(
+          handleKeydown(
             desktopIconsRefAtom,
             desktopIconHighlightAtom,
             startAtom,
@@ -83,7 +203,7 @@ const Root: FC = ({ children }) => {
           setStartAtom(!startAtom);
           break;
         default:
-          keydownDefaultHandler(
+          handleDefaultKeydown(
             e.key,
             desktopIconHighlightAtom,
             setDesktopIconHighlightAtom,
