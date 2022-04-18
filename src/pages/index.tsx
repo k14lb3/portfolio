6bba930af44d349e295d01519e2d399300e47b58
k14lb3 <k14lb3@gmail.com>
2022-04-18 13:17:53 +0800

    Remove focus on window if clicked somewhere that is not a Window component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
 import { NextPage } from "next";
-import { useSetRecoilState } from "recoil";
-import { desktopIconHighlightState, startState } from "@/recoil/atoms";
+import { useResetRecoilState, useSetRecoilState } from "recoil";
+import {
+  desktopIconHighlightState,
+  focusedWindowState,
+  startState,
+} from "@/recoil/atoms";
 import { Background, Icons, Windows } from "@/components/desktop";
 import Taskbar from "@/components/taskbar";
 
@@ -8,6 +12,7 @@ const Desktop: NextPage = () => {
   const setDesktopIconHighlightAtom = useSetRecoilState(
     desktopIconHighlightState
   );
+  const resetFocusedWindowAtom = useResetRecoilState(focusedWindowState);
   const setStartAtom = useSetRecoilState(startState);
 
   return (
@@ -16,14 +21,26 @@ const Desktop: NextPage = () => {
         className="absolute inset-0"
         onClick={() => {
           setDesktopIconHighlightAtom(0);
+          resetFocusedWindowAtom();
           setStartAtom(false);
         }}
       />
-      <div className="w-fit ml-[1.1994vh]" onClick={() => setStartAtom(false)}>
+      <div
+        className="w-fit ml-[1.1994vh]"
+        onClick={() => {
+          setStartAtom(false);
+          resetFocusedWindowAtom();
+        }}
+      >
         <Icons />
       </div>
       <Windows />
-      <Taskbar />
+      <Taskbar
+        onClick={() => {
+          setDesktopIconHighlightAtom(0);
+          resetFocusedWindowAtom()
+        }}
+      />
     </Background>
   );
 };
