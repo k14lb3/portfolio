939eb836e1f16c195d6fa677f7a31fd32fb07963
k14lb3 <k14lb3@gmail.com>
2022-04-18 13:17:03 +0800

    Add props, and move onClick to index page

--- src/components/taskbar/index.tsx
+++ src/components/taskbar/index.tsx
@@ -1,18 +1,14 @@
-import { FC } from "react";
-import { useSetRecoilState } from "recoil";
-import { desktopIconHighlightState } from "@/recoil/atoms";
+import { FC, HTMLAttributes, DetailedHTMLProps } from "react";
 import { Start } from "./start";
 import { Clock } from "./clock";
 
-const Taskbar: FC = () => {
-  const setDesktopIconHighlightAtom = useSetRecoilState(
-    desktopIconHighlightState
-  );
-
+const Taskbar: FC<
+  DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>
+> = ({ ...rest }) => {
   return (
     <div
       className="absolute bottom-0 h-[4.5vh] w-full bg-[#C0C0C0] z-[999]"
-      onClick={() => setDesktopIconHighlightAtom(0)}
+      {...rest}
     >
       <div className="bg-[#DFDFDF] h-[0.152vh] w-full" />
       <div className="bg-white h-[0.152vh] w-full" />
