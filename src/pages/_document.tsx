9d8952f34e8bebc337f8e5174e72d715e076e2e0
k14lb3 <k14lb3@gmail.com>
2022-04-20 10:55:47 +0800

    Add social link preview

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,15 @@ const Document = () => {
     <Html>
       <Head>
         <link rel="icon" href="/favicon.ico" />
+        <meta property="og:url" content="https://k14lb3.vercel.app" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="https://k14lb3.vercel.app/static/images/me.jpg"
+        />
+        <meta property="og:title" content="Karl Ivan Alberto" />
+        <meta property="og:description" content="Software Developer" />
+        <meta name="twitter:card" content="summary" />
       </Head>
       <body>
         <Main />
