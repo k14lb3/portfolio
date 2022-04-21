1cba130da3c4e458ffc68417dbf31a9a585c206e
k14lb3 <k14lb3@gmail.com>
2022-04-21 23:50:49 +0800

    Add taskbar thingy that represents as the windows opened (but only for type content windows)

--- src/components/taskbar/index.tsx
+++ src/components/taskbar/index.tsx
@@ -1,6 +1,16 @@
 import { FC } from "react";
-import { useResetRecoilState } from "recoil";
-import { desktopIconHighlightState, focusedWindowState } from "@/recoil/atoms";
+import {
+  useRecoilState,
+  useRecoilValue,
+  useResetRecoilState,
+  useSetRecoilState,
+} from "recoil";
+import {
+  desktopIconHighlightState,
+  windowsState,
+  topMostWindowState,
+  focusedWindowState,
+} from "@/recoil/atoms";
 import { Start } from "./start";
 import { Clock } from "./clock";
 
@@ -9,6 +19,10 @@ const Taskbar: FC = () => {
     desktopIconHighlightState
   );
   const resetFocusedWindowAtom = useResetRecoilState(focusedWindowState);
+  const [focusedWindowAtom, setFocusedWindowAtom] =
+    useRecoilState(focusedWindowState);
+  const setTopMostWindowAtom = useSetRecoilState(topMostWindowState);
+  const windowsAtom = useRecoilValue(windowsState);
 
   return (
     <div
@@ -22,6 +36,62 @@ const Taskbar: FC = () => {
             onMouseDown={() => resetFocusedWindowAtom()}
           />
           <Start />
+          <div className="relative flex flex-grow items-center space-x-[0.4498vh] h-full mx-[0.5997vh]">
+            {windowsAtom.map(
+              ({ props }) =>
+                props.type === "explorer" && (
+                  <div
+                    key={`${props.title}-task`}
+                    className={`h-[3.3vh] aspect-[80/11] border-solid border-[0.1vh] ${
+                      focusedWindowAtom === props.title
+                        ? "border-white border-t-black border-l-black"
+                        : "border-black border-t-white border-l-white"
+                    }`}
+                    onClick={() => {
+                      setFocusedWindowAtom(props.title);
+                      setTopMostWindowAtom(props.title);
+                    }}
+                  >
+                    <div
+                      className={`h-full border-solid border-[0.1vh] ${
+                        focusedWindowAtom === props.title
+                          ? "pt-[0.1522vh] border-[#DFDFDF] border-t-[#808080] border-l-[#808080] bg-white"
+                          : "border-[#808080] border-t-[#DFDFDF] border-l-[#DFDFDF]"
+                      }`}
+                    >
+                      <div
+                        className={`flex items-center h-full w-full px-[0.4498vh] ${
+                          focusedWindowAtom === props.title
+                            ? "bg-checkered"
+                            : "bg-[#C0C0C0]"
+                        }`}
+                      >
+                        <div className="h-[2.4vh] aspect-square">
+                          <img
+                            className="h-full"
+                            src={
+                              props.icon === "default"
+                                ? "/static/images/icons/folder-opened.png"
+                                : props.icon.src
+                            }
+                            alt={props.title}
+                          />
+                        </div>
+                        <div
+                          className={`h-full ml-[0.4498vh] text-[1.75vh] ${
+                            focusedWindowAtom === props.title
+                              ? "font-bold"
+                              : "pt-[0.2998vh] "
+                          }`}
+                        >
+                          {props.title}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                )
+            )}
+          </div>
           <Clock />
         </div>
       </div>
