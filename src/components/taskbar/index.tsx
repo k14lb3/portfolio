9f42283bd96f28a7d473b5c693367fa3c74068ec
k14lb3 <k14lb3@gmail.com>
2022-04-29 00:10:24 +0800

    Refractor focusedAtom conditional className, and make task toggleable

--- src/components/taskbar/index.tsx
+++ src/components/taskbar/index.tsx
@@ -44,45 +44,51 @@ const Taskbar: FC = () => {
   }, [setFocusedAtom, setStartAtom]);
 
   return (
-    <div
-      className="absolute bottom-0 h-[4.5vh] w-full bg-[#C0C0C0] border-solid border-t-[0.1vh] border-[#DFDFDF] z-[999]"
-      onMouseDown={() => setFocusedAtom("taskbar")}
-    >
+    <div className="absolute bottom-0 h-[4.5vh] w-full bg-[#C0C0C0] border-solid border-t-[0.1vh] border-[#DFDFDF] z-[999]">
       <div className="h-full border-solid border-t-[0.1vh] border-white">
         <div className="flex items-center justify-between relative h-full w-full px-[0.2999vh]">
           <div
             className="absolute inset-0"
-            onMouseDown={() => resetFocusedAtom()}
+            onMouseDown={() => setFocusedAtom("taskbar")}
           />
           <Start />
           <div className="relative flex flex-grow items-center space-x-[0.4498vh] h-full mx-[0.5997vh]">
-            {windowsAtom.map(
-              ({ props }) =>
+            <div
+              className="absolute inset-0"
+              onMouseDown={() => setFocusedAtom("taskbar")}
+            />
+            {windowsAtom.map(({ props }) => {
+              const focused = focusedAtom === props.title;
+              return (
                 props.type === "explorer" && (
                   <div
                     key={`${props.title}-task`}
-                    className={`h-[3.3vh] aspect-[80/11] border-solid border-[0.1vh] ${
-                      focusedAtom === props.title
+                    className={`relative h-[3.3vh] aspect-[80/11] border-solid border-[0.1vh] ${
+                      focused
                         ? "border-white border-t-black border-l-black"
                         : "border-black border-t-white border-l-white"
                     }`}
                     onClick={() => {
-                      setFocusedAtom(props.title as FocusedState);
-                      setTopMostWindowAtom(props.title);
+                      if (focusedAtom !== props.title) {
+                        setFocusedAtom(props.title as FocusedState);
+                        setTopMostWindowAtom(props.title);
+
+                        return;
+                      }
+
+                      setFocusedAtom("taskbar");
                     }}
                   >
                     <div
                       className={`h-full border-solid border-[0.1vh] ${
-                        focusedAtom === props.title
+                        focused
                           ? "pt-[0.1522vh] border-[#DFDFDF] border-t-[#808080] border-l-[#808080] bg-white"
                           : "border-[#808080] border-t-[#DFDFDF] border-l-[#DFDFDF]"
                       }`}
                     >
                       <div
                         className={`flex items-center h-full w-full px-[0.4498vh] ${
-                          focusedAtom === props.title
-                            ? "bg-checkered"
-                            : "bg-[#C0C0C0]"
+                          focused ? "bg-checkered" : "bg-[#C0C0C0]"
                         }`}
                       >
                         <div className="flex items-center h-[2.4vh] aspect-square">
@@ -98,9 +104,7 @@ const Taskbar: FC = () => {
                         </div>
                         <div
                           className={`h-full ml-[0.4498vh] text-[1.75vh] ${
-                            focusedAtom === props.title
-                              ? "font-bold"
-                              : "pt-[0.2998vh] "
+                            focused ? "font-bold" : "pt-[0.2998vh] "
                           }`}
                         >
                           {props.title
@@ -116,7 +120,8 @@ const Taskbar: FC = () => {
                     </div>
                   </div>
                 )
-            )}
+              );
+            })}
           </div>
           <Clock />
         </div>
